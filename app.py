91353314266ecb6c8c1157b42b93920834a4b27f
Jan Earl Oliver Nonesco <jan.nonesco@swapoolabs.com>
2021-12-23 09:51:10 +0800

    Adds run.py

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'janny'
 api = Api(app) #adds resources easily
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 jwt = JWT(app, authenticate, identity_function)
 
