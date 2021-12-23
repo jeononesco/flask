863e6020afb1839da6accf5320d4466744181e54
Jan Earl Oliver Nonesco <jan.nonesco@swapoolabs.com>
2021-12-23 10:40:20 +0800

    Adds postgres connection in app.py

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'janny'
 api = Api(app) #adds resources easily
